c4ecb58b70bcaed361dfa05148bacaf686446a97
Stephan Hügel <urschrei@gmail.com>
2010-10-11 21:38:19 +0100

    Added some info output, better error handling, tidied docstring

--- Utilities/multicite.py
+++ Utilities/multicite.py
@@ -2,57 +2,67 @@
 # encoding: utf-8
 
 """
-Alters sequential instances of \autocite{} into a single instance of \autocites{}, with the contents of subsequent instances inserted into the new "parent"
+Alters sequential instances of \autocite{} into a single instance of
+\autocite{}, with the contents of subsequent instances inserted into the new
+"parent"
 
 Example:
 before: \autocite[1]{Kidd:1988aa}\autocite[2]{Adams:1988aa}
-after: \autocites[1]{Kidd:1988aa}[2]{Adams:1988aa}
+after: \autocite[1]{Kidd:1988aa}[2]{Adams:1988aa}
 
-Takes one file as input, processes it, and outputs it
+Takes exactly one file as input with the -f flag, processes it, and outputs it
+This cleanup is no longer strictly necessary when using the biblatex package
+(and packages which derive from it) which ships with TeX live 2010.
 """
 
 import sys
 import re
 import argparse
-import logging
-import traceback
 
 # define command-line arguments
-parser = argparse.ArgumentParser\
-(description='Fix multiple autocite instances')
-parser.add_argument("-file", metavar = "filename",
-help = "the full path to a text file", required = True,
-type = argparse.FileType("r",0))
-fromcl = parser.parse_args()
-
+try:
+    parser = argparse.ArgumentParser\
+    (description='Fix multiple autocite instances')
+    parser.add_argument("-file", metavar = "filename",
+    help = "the full path to a text file", required = True,
+    type = argparse.FileType("r",0))
+    fromcl = parser.parse_args()
+except IOError:
+    print 'Couldn\'t open specified file!'
+    raise
 
 def fix_cites(fname = None):
     """
-    Fix citations using regex and a devious list comprehension
+    Tidy citations using regex and a devious list comprehension
     """
+    print 'Processing citations in %s' % fname.name
     try:
         with fname:
             lines = [line for line in fname]
     except IOError:
-        raise
+        print 'Can\'t read from file %s' % fname.name
     nc = []
-    comp = re.compile(r'\\autocite')
+    comp = re.compile(r'\\(auto)?cite')
     for l in lines:
         if comp.search(l):
             s = l.split('\\autocite')
             nc.append('%s\\autocite%s' % (s[0], ''.join(s[1:])))
         else:
             nc.append(l)
-    with open(fname.name, 'w') as f:
-        for out_line in nc:
-            f.write(out_line)
+    try:
+        with open(fname.name, 'w') as f:
+            for out_line in nc:
+                f.write(out_line)
+    except IOError:
+        print 'Can\'t write to file %s' % fname.name
+        raise
+    print 'Done. Processed %s line(s)' % len(nc)
+
 
 def main():
     """ main function.
     """
-print "Processing citations"
-fix_cites(fromcl.file)
-print "Finished processing citations"
+    fix_cites(fromcl.file)
 
 
 if __name__ == "__main__":
@@ -63,7 +73,7 @@ if __name__ == "__main__":
         raise
     except Exception, error:
         # all other exceptions, so display the error
-        print "Stack trace:\n", traceback.print_exc(file = sys.stdout)
+        print error
     else:
         pass
     finally:
